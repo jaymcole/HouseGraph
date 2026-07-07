58eee8a696a15c496a0e37044bf1d0106b558013
Jason Cole <jaymcole2k@gmail.com>
2026-07-06 22:31:29 -0700

    new converters

---
+++ src/main/java/io/github/jaymcole/housegraph/graph/nodes/converters/DoubleToStringNode.java
@@ -0,0 +1,39 @@
+package io.github.jaymcole.housegraph.graph.nodes.converters;
+
+import io.github.jaymcole.housegraph.annotations.Display;
+import io.github.jaymcole.housegraph.graph.BaseNode;
+import io.github.jaymcole.housegraph.graph.FlowPort;
+import io.github.jaymcole.housegraph.graph.NodeVariable;
+
+@Display.Name("Double to String")
+public class DoubleToStringNode extends BaseNode {
+
+    private final NodeVariable<Double> in = new NodeVariable<>("in", Double.class, false);
+    private final NodeVariable<String> out = new NodeVariable<>("out", String.class, false);
+
+
+    @Override
+    public void process() {
+        out.setValue(in.getValue().toString());
+    }
+
+    @Override
+    public void configureInputs() {
+        addInput(in);
+    }
+
+    @Override
+    public void configureOutputs() {
+        addOutput(out);
+    }
+
+    @Override
+    public void configureFlowInputs() {
+        addFlowInput(new FlowPort("", FlowPort.Direction.IN));
+    }
+
+    @Override
+    public void configureFlowOutputs() {
+        addFlowOutput(new FlowPort("", FlowPort.Direction.OUT));
+    }
+}
---
+++ src/main/java/io/github/jaymcole/housegraph/graph/nodes/converters/IntegerToStringNode.java
@@ -0,0 +1,39 @@
+package io.github.jaymcole.housegraph.graph.nodes.converters;
+
+import io.github.jaymcole.housegraph.annotations.Display;
+import io.github.jaymcole.housegraph.graph.BaseNode;
+import io.github.jaymcole.housegraph.graph.FlowPort;
+import io.github.jaymcole.housegraph.graph.NodeVariable;
+
+@Display.Name("Integer to String")
+public class IntegerToStringNode extends BaseNode {
+
+    private final NodeVariable<Integer> in = new NodeVariable<>("in", Integer.class, false);
+    private final NodeVariable<String> out = new NodeVariable<>("out", String.class, false);
+
+
+    @Override
+    public void process() {
+        out.setValue(in.getValue().toString());
+    }
+
+    @Override
+    public void configureInputs() {
+        addInput(in);
+    }
+
+    @Override
+    public void configureOutputs() {
+        addOutput(out);
+    }
+
+    @Override
+    public void configureFlowInputs() {
+        addFlowInput(new FlowPort("", FlowPort.Direction.IN));
+    }
+
+    @Override
+    public void configureFlowOutputs() {
+        addFlowOutput(new FlowPort("", FlowPort.Direction.OUT));
+    }
+}
